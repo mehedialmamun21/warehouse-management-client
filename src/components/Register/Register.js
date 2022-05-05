d943c1347c6cef723a9c862a7c3bb061f4eebe23
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-06 05:54:53 +0600

    register section added

---
+++ src/components/Register/Register.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+import app from '../../firebase.init';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { Link, useNavigate } from 'react-router-dom';
+
+const auth  = getAuth(app);
+
+const Register = () => {
+
+    const [validated, setValidated] = useState(false);
+
+    const [success, setSuccess] = useState('');
+
+    const [error, setError] = useState('');
+
+    const [email,setEmail] = useState('');
+
+    const [password,setPassword] = useState('');
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate("/login");
+    };
+
+    const handleFormSubmit = event => {
+
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+            return;
+        }
+
+        setError('');
+        if(!/.{8,}/.test(password)){
+            setError('Have to be minimum 8 in length');
+            return;
+        }
+
+        setError('');
+        if(!/(?=.*?[#?!@$%^&*-])/.test(password)){
+            setError('Password should contain at least one special character');
+            return;
+        }
+
+        setValidated(true);
+
+        createUserWithEmailAndPassword (auth,email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+
+            setSuccess("Congratulations!! registration successful..")
+
+        })
+        .catch(error => {
+            setSuccess('');
+            console.error(error);
+            setError(error.message);
+        })
+
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <div className="registration w-50 mx-auto mt-5">
+                <h2 className='text-primary'>Please Register</h2>
+
+                <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />    
+                    </Form.Group>
+
+                    <p className='text-danger'>{error}</p>
+
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                    <br /> 
+                    <p className='text-success'>{success}</p>
+                    <br />
+
+                    <p>
+                        Already have an account?
+                        <Link
+                            to={"/login"}
+                            className="text-danger text-decoration-none"
+                            onClick={navigateLogin}
+                            >
+                            Please Login
+                        </Link>
+                    </p>
+
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuUp995lP7BlbXMKnTJKDMobv_7juTM7I",
+  authDomain: "smartphone-warehouse-7f130.firebaseapp.com",
+  projectId: "smartphone-warehouse-7f130",
+  storageBucket: "smartphone-warehouse-7f130.appspot.com",
+  messagingSenderId: "1038651961079",
+  appId: "1:1038651961079:web:f42c17a6fc39e752086df2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
