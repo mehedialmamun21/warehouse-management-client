b667f8d9e5d8fe5760ae5dd15b365fed87763019
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-08 05:34:42 +0600

    my-items section added with all inventories

--- src/components/Inventories/Inventories.js
+++ src/components/Inventories/Inventories.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Products from '../Products/Products';
+import useInventories from '../hooks/useInventories';
 import './Inventories.css';
+
+
 const Inventories = () => {
 
+    const [products] = useInventories();
+
     const navigate = useNavigate();
+    
     const navigateToAddItem = () =>{
         navigate(`/add_item`);
     }
 
     return (
-        <div className='inventory'>
+        
+        <div>
 
             <button onClick={navigateToAddItem} className='btn btn-success ms-5 mt-5'>Add New Item</button>
 
-            <Products></Products>
+            <div className='pro-contain'>
+
+                {
+                    products.map(product => <div key={product._id}>
+                            <div className="pro-all">
+                                <img src={product.img} alt="" />
+                                <h4>Name : {product.name}</h4>
+                                <p>Price : {product.price}</p>
+                                <p><small>{product.description}</small></p>
+                                <button className='btn btn-danger' >Delete</button>
+                            </div>
+                        </div> )
+                }
+
+            </div>
 
         </div>
+
+
     );
 };
 
--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import useInventories from '../hooks/useInventories';
+import './MyItems.css';
 
 const MyItems = () => {
+
+    const [products] = useInventories();
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are You Sure ?');
+        if(proceed){
+
+        }
+    }
+
     return (
-        <div>
-            <h2>From My Items section</h2>
+        <div className='pro-container'>
+
+            {
+                products.map(product => <div key={product._id}>
+                        <div className="product-all">
+                            <img src={product.img} alt="" />
+                            <h4>Name : {product.name}</h4>
+                            <p>Price : {product.price}</p>
+                            <p><small>{product.description}</small></p>
+                            <button onClick={() => handleDelete(product._id)} className='btn btn-danger' >Delete</button>
+                        </div>
+                    </div> )
+            }
+
         </div>
     );
 };
